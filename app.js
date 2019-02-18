c844cb6e4d453b17bafeac664379cbae4301ba03
kamranferoz <kamranferoz@yahoo.com>
2019-02-18 21:02:57 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -33,4 +33,9 @@ app.get(/^\/(\d{5})$/, function (req, res, next) {
 app.use(function (req, res) {
     res.status(404).render("404");
 });
-app.listen(8080);
+
+// app.listen(8080);
+
+var port = process.env.PORT || 8080;
+var server=app.listen(port,function() {
+    console.log("app running on port 8080"); });
